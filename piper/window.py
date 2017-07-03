8d77e4bf6c8205a6d2aa9783e2fb99c2cae1b47e
Jente Hidskes <hjdskes@gmail.com>
2017-07-03 16:54:08 +0200

    Window: use GtkTemplate to instantiate from a GtkBuilder template
    In C/GTK+, templates allow one to define the UI in .ui files, from which
    implementations can be instantiated. This significantly reduces
    boilerplate code that creates widgets, sets their desired properties and
    connects their signals[1, 2].
    
    For several reasons, this does not work in Python[3]. gi_composites.py[4] is an
    implementation in Python that does make this work. It's been used by
    several projects for a few months now and none report issues. See the
    project's README for more information[5].
    
    Taking these facts into mind, let's experiment for a bit and see how it
    can serve Piper.
    
    [1]: https://wiki.gnome.org/HowDoI/CustomWidgets#Templates
    [2]: https://blogs.gnome.org/tvb/2013/04/09/announcing-composite-widget-templates/
    [3]: https://bugzilla.gnome.org/show_bug.cgi?id=701843
    [4]: https://github.com/virtuald/pygi-composite-templates/
    [5]: https://github.com/virtuald/pygi-composite-templates/blob/master/README.rst

--- piper/window.py
+++ piper/window.py
@@ -14,20 +14,21 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from gettext import gettext as _
+from .gi_composites import GtkTemplate
 
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-from .mousemap import MouseMap
 
-
+@GtkTemplate(ui="/org/freedesktop/Piper/window.ui")
 class Window(Gtk.ApplicationWindow):
     """A Gtk.ApplicationWindow subclass to implement the main application
     window."""
 
     __gtype_name__ = "ApplicationWindow"
+
+    stack = GtkTemplate.Child()
 
     def __init__(self, ratbag, *args, **kwargs):
         """Instantiates a new Window.
@@ -35,42 +36,10 @@ class Window(Gtk.ApplicationWindow):
         @param ratbag The ratbag instance to connect to, as ratbagd.Ratbag
         """
         Gtk.ApplicationWindow.__init__(self, *args, **kwargs)
+        self.init_template()
+
         self._ratbag = ratbag
-
-        stack = Gtk.Stack()
-        self.add(stack)
-        stack.props.homogeneous = True
-        stack.props.transition_duration = 500
-        stack.props.transition_type = Gtk.StackTransitionType.SLIDE_LEFT_RIGHT
-
-        device = self._fetch_ratbag_device()
-        stack.add_titled(self._setup_buttons_page(device), "buttons", _("Buttons"))
-        self.set_titlebar(self._setup_headerbar(stack))
-
-    def _setup_headerbar(self, stack):
-        headerbar = Gtk.HeaderBar()
-
-        sizeGroup = Gtk.SizeGroup.new(Gtk.SizeGroupMode.HORIZONTAL)
-        self._quit = Gtk.Button.new_with_mnemonic(_("_Quit"))
-        self._quit.connect("clicked", lambda button, data: data.destroy(), self)
-        sizeGroup.add_widget(self._quit)
-        headerbar.pack_start(self._quit)
-
-        switcher = Gtk.StackSwitcher()
-        switcher.set_stack(stack)
-        headerbar.set_custom_title(switcher)
-
-        return headerbar
-
-    def _setup_buttons_page(self, device):
-        mousemap = MouseMap("#Buttons", device, spacing=20, border_width=20)
-        sizegroup = Gtk.SizeGroup(Gtk.SizeGroupMode.HORIZONTAL)
-        profile = device.active_profile
-        for button in profile.buttons:
-            mapbutton = Gtk.Button("Button {}".format(button.index))
-            mousemap.add(mapbutton, "#button{}".format(button.index))
-            sizegroup.add_widget(mapbutton)
-        return mousemap
+        self._device = self._fetch_ratbag_device()
 
     def _fetch_ratbag_device(self):
         """Get the first ratbag device available. If there are multiple
