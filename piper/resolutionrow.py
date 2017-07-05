3213b6bd62c6008be9d876e79ee4f0ab65a1c6aa
Jente Hidskes <hjdskes@gmail.com>
2017-07-05 16:09:01 +0200

    Window: add list of resolutions
    This list can set a profile's resolutions. Currently, it is limited to
    the active profile because there is no support for profiles other than
    the default.
    
    Adding and removing resolutions is not yet implemented because this is
    not yet exposed through libratbag (see [1]).
    
    [1]: https://github.com/libratbag/libratbag/issues/211

---
+++ piper/resolutionrow.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2017 Jente Hidskes <hjdskes@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from .gi_composites import GtkTemplate
+from .ratbagd import RatbagdResolution
+
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+@GtkTemplate(ui="/org/freedesktop/Piper/resolutionRow.ui")
+class ResolutionRow(Gtk.ListBoxRow):
+    """A Gtk.ListBoxRow subclass containing the widgets to configure a
+    resolution."""
+
+    __gtype_name__ = "ResolutionRow"
+
+    index_label = GtkTemplate.Child()
+    title_label = GtkTemplate.Child()
+    revealer = GtkTemplate.Child()
+    frame_y = GtkTemplate.Child()
+    label_x = GtkTemplate.Child()
+    scale_x = GtkTemplate.Child()
+    scale_y = GtkTemplate.Child()
+
+    def __init__(self, ratbagd_resolution, *args, **kwargs):
+        Gtk.ListBoxRow.__init__(self, *args, **kwargs)
+        self.init_template()
+        self._resolution = ratbagd_resolution
+        self._separate_xy = RatbagdResolution.CAP_SEPARATE_XY_RESOLUTION in \
+            ratbagd_resolution.capabilities
+        self._handler = self._resolution.connect("notify::resolution",
+                                                 self._on_resolution_changed)
+        self._init_values()
+
+    def _init_values(self):
+        # Initializes the scales and the title label and sets the Y resolution
+        # configuration visible if it's supported by the device.
+        (xres, yres) = self._resolution.resolution
+        minres = self._resolution.minimum
+        maxres = self._resolution.maximum
+
+        self.index_label.set_text("Resolution {}".format(self._resolution.index))
+
+        self.scale_x.props.adjustment = Gtk.Adjustment(xres, minres, maxres)
+        self.scale_x.set_value(xres)
+        if self._separate_xy:
+            self.scale_y.props.adjustment = Gtk.Adjustment(yres, minres, maxres)
+            self.scale_y.set_value(yres)
+            self.frame_y.set_visible(True)
+        else:
+            self.label_x.set_text("Resolution")
+
+    @GtkTemplate.Callback
+    def _on_delete_button_clicked(self, button):
+        print("TODO: RatbagdProfile needs a way to delete resolutions")
+
+    @GtkTemplate.Callback
+    def _on_value_changed(self, scale):
+        # The scale has been moved, update RatbagdResolution's resolution and
+        # the title label.
+        xres = int(self.scale_x.get_value())
+        yres = int(self.scale_y.get_value())
+
+        # Freeze the notify::resolution signal from firing to prevent Piper from
+        # ending up in an infinite update loop.
+        with self._resolution.handler_block(self._handler):
+            self._resolution.resolution = (xres, yres)
+
+        if self._separate_xy:
+            title = "{} DPI (X), {} DPI (Y)".format(xres, yres)
+        else:
+            title = "{} DPI".format(xres)
+        self.title_label.set_text(title)
+
+    def _on_resolution_changed(self, obj, pspec):
+        # RatbagdResolution's resolution has changed, update the scales.
+        (xres, yres) = self._resolution.resolution
+        self.scale_x.set_value(xres)
+        self.scale_y.set_value(yres)
+
+    def toggle_revealer(self):
+        """Toggles the revealer to show or hide the configuration widgets."""
+        reveal = not self.revealer.get_reveal_child()
+        self.revealer.set_reveal_child(reveal)
--- piper/window.py
+++ piper/window.py
@@ -15,6 +15,9 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 from .gi_composites import GtkTemplate
+from .mousemap import MouseMap
+from .ratbagd import RatbagErrorCode
+from .resolutionrow import ResolutionRow
 
 import gi
 gi.require_version("Gtk", "3.0")
@@ -29,6 +32,8 @@ class Window(Gtk.ApplicationWindow):
     __gtype_name__ = "ApplicationWindow"
 
     stack = GtkTemplate.Child()
+    listbox = GtkTemplate.Child()
+    add_resolution_row = GtkTemplate.Child()
 
     def __init__(self, ratbag, *args, **kwargs):
         """Instantiates a new Window.
@@ -41,11 +46,27 @@ class Window(Gtk.ApplicationWindow):
         self._ratbag = ratbag
         self._device = self._fetch_ratbag_device()
 
+        self._setup_resolutions_page()
+
+    def _setup_resolutions_page(self):
+        # TODO: mousemap needs to show which button switches resolution
+        mousemap = MouseMap("#Device", self._device, spacing=20, border_width=20)
+
+        page = self.stack.get_child_by_name("resolutions")
+        page.pack_start(mousemap, True, True, 0)
+        # Place the MouseMap on the left
+        page.reorder_child(mousemap, 0)
+
+        for resolution in profile.resolutions:
+            row = ResolutionRow(resolution)
+            self.listbox.insert(row, resolution.index)
+
     def _fetch_ratbag_device(self):
         """Get the first ratbag device available. If there are multiple
         devices, an error message is printed and we default to the first
         one. Otherwise, an error is shown and we return None.
         """
+        # TODO: replace with better implementation once we go for the welcome screen.
         if len(self._ratbag.devices) == 0:
             print("Could not find any devices. Do you have anything vaguely mouse-looking plugged in?")
             return None
@@ -54,3 +75,16 @@ class Window(Gtk.ApplicationWindow):
             for d in self._ratbag.devices[1:]:
                 print("Ignoring device {}".format(d.name))
         return self._ratbag.devices[0]
+
+    @GtkTemplate.Callback
+    def _on_row_activated(self, listbox, row):
+        if row is self.add_resolution_row:
+            print("TODO: RatbagdProfile needs a way to add resolutions")
+        elif row is not None:
+            row.toggle_revealer()
+
+    @GtkTemplate.Callback
+    def _on_save_button_clicked(self, button):
+        status = self._device.commit()
+        if not status == RatbagErrorCode.RATBAG_SUCCESS:
+            print("TODO: inform user of error")
